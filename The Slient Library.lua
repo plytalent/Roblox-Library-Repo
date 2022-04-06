4f9821dd75a0ef5fa7a8b84ff4a6c7055db1aaf1
plytalent <39195826+plytalent@users.noreply.github.com>
2022-04-06 11:26:23 +0700

    Update The Slient Library.lua

--- The Slient Library.lua
+++ The Slient Library.lua
@@ -7,7 +7,9 @@ local real_func = {
     print = print,
     tostring = tostring
 }
-
+local tostring = nil
+local print = nil
+local debug = nil
 local namepool = {}
 
 local random = Random.new()
@@ -55,68 +57,68 @@ end
 function scheduler.new()
     local self = setmetatable({},scheduler)
     self.loop = game:GetService("RunService").Stepped
-    self.__internal_event__ = {}
-    self.__internal_var__ = {}
-    self.__internal_func__ = {}
+    self._internal_event_ = {}
+    self._internal_var_ = {}
+    self._internal_func_ = {}
     self.funcs = {}
-    self.__internal_var__.Changing_event = false
-    self.__internal_var__.Executed_Function = true
-    self.__internal_var__.execution_index = 1
-    self.__internal_var__.RandomString = getRandomString()
-    self.__internal_func__.bindtoloop = function()
-        self.__internal_event__.MainEventLoop = self.loop:Connect(function(delta)
+    self._internal_var_.Changing_event = false
+    self._internal_var_.Executed_Function = true
+    self._internal_var_.execution_index = 1
+    self._internal_var_.RandomString = getRandomString()
+    self._internal_func_.bindtoloop = function()
+        self._internal_event_.MainEventLoop = self.loop:Connect(function(delta)
             local start_index = 1
-            if self.__internal_var__.execution_index > 1 then
-                start_index = self.__internal_var__.execution_index
+            if self._internal_var_.execution_index > 1 then
+                start_index = self._internal_var_.execution_index
             end
             local delta = tick()
             for i=start_index , #self.funcs do
                 if self.funcs[i] then
-                    self.__internal_var__.Executed_Function = false
+                    self._internal_var_.Executed_Function = false
                     self.funcs[i]()
-                    self.__internal_var__.Executed_Function = true
-                    debug("[Scheduler Event]", "[", self.__internal_var__.RandomString, "]", "Function Took", (tick()-delta)*1000, "ms to Execute")
+                    self._internal_var_.Executed_Function = true
+                    debug("[Scheduler Event]", "[", self._internal_var_.RandomString, "]", "Function Took", (tick()-delta)*1000, "ms to Execute")
                     delta = tick()
-                    self.__internal_var__.execution_index = i
-                    if self.__internal_var__.Changing_event then
+                    self._internal_var_.execution_index = i
+                    if self._internal_var_.Changing_event then
                         break
                     end  
                 end
             end
         end)
-        debug("[Scheduler]", "[", self.__internal_var__.RandomString, "]", "Binded To New Loop")
+        debug("[Scheduler]", "[", self._internal_var_.RandomString, "]", "Binded To New Loop")
     end
-    self.__internal_func__.bindtoloop()
-    debug("[Scheduler]", "[", self.__internal_var__.RandomString, "]", "Initialized")
+    self._internal_func_.bindtoloop()
+    debug("[Scheduler]", "[", self._internal_var_.RandomString, "]", "Initialized")
     return self
 end
 function scheduler:Add_Func(self,func,args)
-    debug("[Scheduler]", "[", self.__internal_var__.RandomString, "]", "Add Function To scheduler")
+    debug("[Scheduler]", "[", self._internal_var_.RandomString, "]", "Add Function To scheduler")
     self.funcs[#self.funcs+1] = func
 end
 function scheduler:Remove_Func(self,func)
     for index=1, #self.funcs do
         if self.funcs[index] == func then
-            debug("[Scheduler]", "[", self.__internal_var__.RandomString, "]", "Remove Function From scheduler")
+            debug("[Scheduler]", "[", self._internal_var_.RandomString, "]", "Remove Function From scheduler")
             self.funcs[index] = nil
         end
     end
 end
 function scheduler:Switch_Event(ev)
     if not ev then
-        debug("[Scheduler]", "[", self.__internal_var__.RandomString, "]", "Switching Event Scheduler")
-        self.__internal_event__.MainEventLoop:Disconnect()
-        self.__internal_var__.Changing_event = true
-        while self.__internal_var__.Executed_Function do
+        debug("[Scheduler]", "[", self._internal_var_.RandomString, "]", "Switching Event Scheduler")
+        self._internal_event_.MainEventLoop:Disconnect()
+        self._internal_var_.Changing_event = true
+        while self._internal_var_.Executed_Function do
             self.loop:Wait()
         end
-        self.__internal_var__.Changing_event = false
+        self._internal_var_.Changing_event = false
         self.loop = ev
-        self.__internal_func__.bindtoloop()
+        self._internal_func_.bindtoloop()
     end
 end
 
-local tostring = function(input)
+tostring = function(input)
     local typeofinput = typeof(input):lower()
     if typeofinput == "instance" or typeofinput == "userdata" then
         local can_get_name, name = pcall(function()
@@ -141,7 +143,7 @@ local tostring = function(input)
     end
     return real_func["tostring"](input)
 end
-local print = function(...)
+print = function(...)
     if libsetting.loglevel > 0 then
         local args = {...}
         local buffer = buffer.new()
@@ -152,7 +154,7 @@ local print = function(...)
         rconsoleinfo(buffer:Read())
     end
 end
-local debug = function(...)
+debug = function(...)
     if libsetting.loglevel > 4 then
         local args = {"[Debug]",...}
         print(args)
